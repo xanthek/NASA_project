684a3117a063e0ad841b6631d01010382f68907f
Janusz Angerman <janusz@mac-mini.local>
2024-05-16 14:30:37 +0200

    added route planets, added scripts

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,24 @@
 const http = require('http');
 
 const app = require('./app.js');
+const {loadPlanetsData} = require('./models/planets.model.js');
 
 const PORT = process.env.PORT || 4000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-})
+async function startServer() {
+
+    try {
+        await loadPlanetsData();
+    }
+    catch(err) {
+        console.log(err);
+    }
+    
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    })
+}
+
+startServer();
