a434782f631c3231535c87f8eb85d1cbe2cc3921
Janusz Angerman <janusz@mac-mini.local>
2024-05-16 16:15:46 +0200

    added logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router.js');
 
 const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
-}))
+}));
+
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..','public')));
 app.use(planetsRouter);
