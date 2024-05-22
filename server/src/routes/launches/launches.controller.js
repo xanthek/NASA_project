1ae0a8597b5b6d0c1675991695966ab403119163
Janusz <janusz.angerman@yahoo.com>
2024-05-22 22:26:21 +0200

    add pm2 and remove consol.logs

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,13 +1,11 @@
 const {getAllLaunches, addNewLaunch, existsLaunchWithId, abortLaunchById} = require('../../models/launches.model.js');
 
 function httpGetAllLaunches(req,res) {
-    console.log('launches');
     return res.status(200).json(getAllLaunches());
 }
 
 function httpAddNewLaunch(req, res) {
     const launch  = req.body;
-    console.log(launch);
     if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
         return res.status(400).json({
             error: 'Missing required launch property'
@@ -16,7 +14,7 @@ function httpAddNewLaunch(req, res) {
 
     launch.launchDate = new Date(launch.launchDate);
     if (isNaN(launch.launchDate)) {
-        res.status(400).json({
+        return res.status(400).json({
             error: 'Invalid launch date'
         });
     }
